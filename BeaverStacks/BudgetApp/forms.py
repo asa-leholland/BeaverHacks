1264ef14674bd7cb2b640b4de090d7f524128774
Charlie Magill <magillri@oregonstate.edu>
2021-04-09 10:46:08 -0600

    Changed function names in views

--- BeaverStacks/BudgetApp/forms.py
+++ BeaverStacks/BudgetApp/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Groups
+from .models import Transactions
 
 
 # This file will be used to create forms for HTML embedding of the data
@@ -7,3 +8,8 @@ class GroupForm(ModelForm):
     class Meta:
         model = Groups
         fields = ['group_description']
+
+class TransactionsForm(ModelForm):
+    class Meta:
+        model = Transactions
+        fields = ['vendor', 'date', 'amount']
--- BeaverStacks/BudgetApp/views.py
+++ BeaverStacks/BudgetApp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .forms import GroupForm
+from .forms import TransactionsForm
+from .models import *
 from django.http import HttpResponse
 
 
@@ -7,4 +9,31 @@ from django.http import HttpResponse
 def index(request):
     form = GroupForm()
     context = {'form': form}
+    return render(request, 'BudgetApp/index.html', context)
+
+def addTransaction(request):
+    form = TransactionsForm()
+    if request.method == 'POST':
+            form = TransactionsForm(request.POST)
+            if form.is_valid():
+                form.save()
+    context = {'form': form}
+    return render(request, 'BudgetApp/index.html', context)
+
+def updateTransaction(request, pk):
+    transaction = Transactions.objects.get(id=pk)
+    form = TransactionsForm(instance=order)
+    if request.method == 'POST':
+            form = TransactionsForm(request.POST, instance=transaction)
+            if form.is_valid():
+                form.save()
+    context = {'form': form}
+    return render(request, 'BudgetApp/index.html', context)
+
+def deleteTransaction(request, pk):
+    transaction = Transactions.objects.get(id=pk)
+    if request.method == "POST":
+        transaction.delete()
+    form = TransactionsForm()
+    context = {'item': transaction}
     return render(request, 'BudgetApp/index.html', context)
