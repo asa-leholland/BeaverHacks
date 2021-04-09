ce32969453d55dfdea8514bcddd1d892c55373e3
Charlie Magill <magillri@oregonstate.edu>
2021-04-08 22:13:13 -0600

    Created Transaction model

--- BeaverStacks/BudgetApp/models.py
+++ BeaverStacks/BudgetApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Transaction(models.Model):
+    transaction_id = models.PositiveIntegerField(default=1)
+    vendor = models.CharField(default='', max_length=50)
+    date = models.DateField(default=date.today)
+    amount = models.DecimalField(default=0.00, max_digits=16, decimal_places=2)
+
+
