1945660b7f3a0528b137e9749f96f8ea74454648
Asa Holland <hollandasa@gmail.com>
2021-04-09 01:58:28 -0700

    Update forms.py
    updated naming convention on model to allow load

--- BeaverStacks/BudgetApp/forms.py
+++ BeaverStacks/BudgetApp/forms.py
@@ -1,9 +1,9 @@
 from django.forms import ModelForm
-from .models import Group
+from .models import Groups
 
 
 # This file will be used to create forms for HTML embedding of the data
 class GroupForm(ModelForm):
     class Meta:
-        model = Group
+        model = Groups
         fields = ['group_description']
