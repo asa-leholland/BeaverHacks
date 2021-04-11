c3af2ca737e474029dfd780a2145af186ac05586
Jacob Ogle <ogleja@oregonstate.edu>
2021-04-11 00:09:18 -0400

    Created new functions in views to generate data from all transactions. Also added getter function to Transactions class in models

--- BeaverStacks/BudgetApp/views.py
+++ BeaverStacks/BudgetApp/views.py
@@ -2,12 +2,14 @@
 # Developed for BeaverHacks Hackathon
 # Developers: Kenneth Street, Asa Holland, Charlie Magill, Jacob Ogle
 """
-
+import re
 
 from .models import Categories, Groups, Budgets, Transactions
 from django.utils import timezone
 from django.shortcuts import render
 from .forms import *
+import plotly.express as px
+import pandas as pd
 
 
 # Create your views here.
@@ -21,15 +23,18 @@ def index(request):
     categories = Categories()
     groups = Groups()
     transactions = Transactions()
-    # budgets = Budgets()
+    budgets = Budgets()
+
+    getting_transaction_data()
+    manipulating_transaction_data()
 
     if request.method == 'POST':
+        if request.POST.get('create_budget'):
+            create_budget(request, budgets)
         if request.POST.get('create_category'):
             create_category(request, categories)
         if request.POST.get('create_group'):
             create_group(request, groups)
-        if request.POST.get('transaction_description'):  #  Change transaction_description to create_transaction
-            create_transaction(request, transactions)
         if request.POST.get('update_category'):
             update_category(request, request.POST.get('primary_key'))
         if request.POST.get('update_group'):
@@ -38,6 +43,8 @@ def index(request):
             delete_category(request, request.POST.get('primary_key'))
         if request.POST.get('delete_group'):
             delete_group(request, request.POST.get('primary_key'))
+        if request.POST.get('create_pie_plot'):
+            create_pie_plot(request)
 
     # this is a list of all the objects in the db
     budgets = Budgets.objects.all()
@@ -142,3 +149,52 @@ def get_month_year_combinations():
     # for budget_item in form:
     #     budget_list.append(str(budget_item.month) + str(budget_item.year))
     return budget_list
+
+
+def getting_transaction_data():
+    print('function called')
+    transactions = Transactions.objects.all()
+    transaction_category = []
+    transaction_amount = []
+    for transaction in transactions:
+        transaction_category.append(transaction.get_category())
+        transaction_amount.append(transaction.get_amount())
+    transaction_data = list(zip(transaction_category, transaction_amount))
+    return transaction_data
+
+
+def manipulating_transaction_data():
+    data = getting_transaction_data()
+    data_groupings = {}
+    for i in data:
+        if i[0] in data_groupings:
+            data_groupings[f'{i[0]}'] += i[1]
+        else:
+            data_groupings[f'{i[0]}'] = i[1]
+
+    data_for_charts = []
+    for key, value in data_groupings.items():
+        temp = [key, value]
+        data_for_charts.append(temp)
+
+    cats = [i[0] for i in data_for_charts]
+    print(cats)
+    amount = [i[1] for i in data_for_charts]
+    amount_filter = []
+    for i in amount:
+        amount_filter.append((re.findall(r'[+-]?([0-9]*[.])?[0-9]+', str(i))))
+
+    print(amount_filter)
+    amount_filter = [float(i[0]) for i in amount_filter]
+    print(amount_filter)
+    final_dict = {'Category': cats, 'Amount': amount_filter}
+    df = pd.DataFrame.from_dict(final_dict)
+    return df
+
+
+def create_pie_plot(request):
+    fig = px.pie(manipulating_transaction_data(), values='Amount', names='Category', title='Spending by Category')
+    fig.show()
+    graph = fig.to_html(full_html=False, default_height=500, default_width=700)
+    return graph
+
